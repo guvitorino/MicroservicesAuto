4fb35de7bb81f59fb668efef10db1b9610141d95
Gustavo <gu_vitorino@hotmail.com>
2020-09-27 20:03:29 -0300

    feat(MS-Boleto): added config to eureka server

--- MS-Boleto/src/main/java/com/vitorino/msboleto/MsBoletoApplication.java
+++ MS-Boleto/src/main/java/com/vitorino/msboleto/MsBoletoApplication.java
@@ -2,8 +2,10 @@ package com.vitorino.msboleto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MsBoletoApplication {
 
 	public static void main(String[] args) {
