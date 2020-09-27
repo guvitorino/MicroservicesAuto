b392c0f67260a0e71cabd20a2d83af25d9ad1b13
Gustavo <gu_vitorino@hotmail.com>
2020-09-27 20:41:03 -0300

    feat(MS-Automoveis): added config to eureka server

--- MS-Automoveis/src/main/java/com/vitorino/msautomoveis/MsAutomoveisApplication.java
+++ MS-Automoveis/src/main/java/com/vitorino/msautomoveis/MsAutomoveisApplication.java
@@ -2,8 +2,10 @@ package com.vitorino.msautomoveis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MsAutomoveisApplication {
 
 	public static void main(String[] args) {
