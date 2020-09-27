1c11869984bc20b2fc550bfa8bcb3ec8361d8015
Gustavo <gu_vitorino@hotmail.com>
2020-09-27 14:48:24 -0300

    feat(MS-Boleto): added params validation to boleto endpoint

--- MS-Boleto/src/main/java/com/vitorino/msboleto/controller/PaymentSlipController.java
+++ MS-Boleto/src/main/java/com/vitorino/msboleto/controller/PaymentSlipController.java
@@ -1,11 +1,19 @@
 package com.vitorino.msboleto.controller;
 
+import javax.validation.Valid;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import com.vitorino.msboleto.model.bo.PaymentSlipBO;
 import com.vitorino.msboleto.model.projection.PaymentSlipProjection;
 import com.vitorino.msboleto.repository.PaymentSlipRepository;
 import com.vitorino.msboleto.service.PaymentSlipService;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -21,8 +29,22 @@ public class PaymentSlipController {
 
     @PostMapping(path = "/boleto", consumes = "application/json", produces = "application/json")
     @ResponseStatus(HttpStatus.CREATED)
-    public PaymentSlipProjection create(@RequestBody PaymentSlipBO paymentSlip) {
+    public PaymentSlipProjection create(@Valid @RequestBody PaymentSlipBO paymentSlip) {
         PaymentSlipService service = new PaymentSlipService();
         return service.save(paymentSlip, this.repository);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        return errors;
     }
 }
--- MS-Boleto/src/main/java/com/vitorino/msboleto/model/bo/PaymentSlipBO.java
+++ MS-Boleto/src/main/java/com/vitorino/msboleto/model/bo/PaymentSlipBO.java
@@ -6,17 +6,23 @@ import lombok.Setter;
 import java.util.Calendar;
 import java.util.Date;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 public class PaymentSlipBO {
     @Getter
     @Setter
+    @NotBlank(message = "marca é obrigatório")
     private String marca;
 
     @Getter
     @Setter
+    @NotBlank(message = "modelo é obrigatório")
     private String modelo;
 
     @Getter
     @Setter
+    @NotNull(message = "valor é obrigatório")
     private Double valor;
 
     @Getter
